e04131bd98c028422a6522a9a4b2dd18e0120a77
mitchellciupak <ciupak@purdue.edu>
2020-09-14 16:22:14 -0400

    Image Disp :zap:

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ success,image = vidcap.read()
 
 count = 0
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("Assets/frame%d.jpg" % count, image)     # save frame as JPEG file
   success,image = vidcap.read()
   print('Read a new frame: ', success)
+  cv2.imshow('image', image)
+  cv2.waitKey(100)
   count += 1
+
+cv2.destroyAllWindows()
