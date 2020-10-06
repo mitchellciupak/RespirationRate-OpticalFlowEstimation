c1041bb408aaebcf76ce4d9bb54bd0dc1dc2d821
mitchellciupak <ciupak@purdue.edu>
2020-10-05 21:26:09 -0400

    opticalFlowRR.py :arrow_up:

--- opticalFlowRR.py
+++ opticalFlowRR.py
@@ -1,138 +1,140 @@
-#Mitchell Ciupak adapted from https://github.com/ynandwan/OpticalFlowRR
-#Imports
-from __future__ import print_function
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import sys, os
+# Estimated Respiration Rate from Lucas-Kanade Optical Flow
+
 import cv2
 import numpy as np
-import scipy as sp
-import sys
-from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
-#Macros
-SIZE = (160, 120)
-GRAD_SIZE = (int(160 * 0.25), int(120 * 0.25))
-FLOW_MEMORY = 0.85
-PFF_MEMORY = 0.85
-MA = 10
-SIGNAL_MEMORY = 0.80
-n = 0
+'''
+    USAGE: optical_flow.py [<video_source>]
+    NOTES: defaults to cam
+'''
 
-#File I/O
-prevGray = None
-rrsignal = []
-file_name = sys.argv[1]
-cap = cv2.VideoCapture(file_name)
-frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+MAX_CORNERS = 200
 
-allrn = []
-while (cap.isOpened()):
-    ret, frame = cap.read()
+if __name__ == '__main__':
+    import sys
 
-    if ret == True:
-        n += 1
-        frame = frame.astype(float)
-        gray_org = 0.29 * frame[:, :, 2] + 0.59 * frame[:, :, 1] + 0.11 * frame[:, :, 0]
-        gray_blur = cv2.GaussianBlur(gray_org, (21, 21), 8, borderType=0)
-        gray = cv2.resize(gray_blur, SIZE)
-        imx, imy = np.gradient(gray)
+    # Import Video
+    try:
+        fptr = sys.argv[1]
+    except:
+        fptr = 0
 
-        if (n == 1):
-            prevGray = gray
+    cap = cv2.VideoCapture(fptr)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    isValid, firstFrame = cap.read()
 
-        dn = prevGray - gray
-        prevGray = gray
-        gradNorm2 = np.multiply(imy, imy) + np.multiply(imx, imx)
-        gradNorm2[gradNorm2 < 9] = float("inf")
-        flowx = np.multiply(imx, dn) / gradNorm2
-        flowy = np.multiply(imy, dn) / gradNorm2
+    if not isValid:
+        quit(0)
 
-        flowx, flowy = cv2.resize(flowx, None, fx=0.25, fy=0.25), cv2.resize(flowy, None, fx=0.25, fy=0.25)
-        fn = np.hstack((flowx.flatten(), flowy.flatten()))
+    # [Detect Corners of First Frame](https://docs.opencv.org/master/d4/d8c/tutorial_py_shi_tomasi.html)
+    ff_gray = cv2.cvtColor(firstFrame,cv2.COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(ff_gray,mask=None,maxCorners=MAX_CORNERS,qualityLevel=0.005,minDistance=10,blockSize=7)
+    #corners = np.int0(corners)
 
-        if (n == 1):
-            tflow = fn
-            dflow = fn
-            continue
-
-        tflow = FLOW_MEMORY * tflow + fn
-        mag = LA.norm(tflow)
-
-        if (mag > MA):
-            tflow = (tflow * MA) / mag
-
-        if (dflow.dot(fn) > 0):
-            dflow = PFF_MEMORY * dflow + fn
-        else:
-            dflow = PFF_MEMORY * dflow - fn
-
-        mag = LA.norm(dflow)
-        if (mag > MA):
-            dflow = (dflow * MA) / mag
-        #
-        pffNorm = LA.norm(dflow)
-        rn = np.dot(tflow, dflow) / pffNorm
-        allrn.append(rn)
-        if (len(rrsignal) > 0):
-            rrsignal.append(rrsignal[-1] * SIGNAL_MEMORY + rn)
-        else:
-            rrsignal.append(0)
-        #
-        if n % 100 == 0:
-            print("n: {0}, this rr: {1}".format(n, rn))
-        #
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Init Vars
+    prevImg_gray = ff_gray.copy()
+    prevFrame = firstFrame.copy()
+    mask = np.zeros_like(firstFrame)
+    frameCount = 1
+    x = []
+    y = []
+    # Measure Change in corners over time
+    while cap.isOpened():
+        isValid, nextFrame = cap.read()
+        if not isValid:
             break
-    else:
-        print("n: {0}".format(n))
-        break
 
-cap.release()
-cv2.destroyAllWindows()
+        frameCount += 1
+        nextImg_gray = cv2.cvtColor(nextFrame, cv2.COLOR_BGR2GRAY)
 
-plot_rrsignal = np.array(rrsignal)
-sigl = len(rrsignal)
-plot_rrsignal = plot_rrsignal[int(sigl * 0.06): int(sigl * 0.94)]
+        # Calculates sparse optical flow, Lucas-Kanade
+        LKargs = dict(winSize=(15, 15), maxLevel=2,criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+        nextP, status, error = cv2.calcOpticalFlowPyrLK(prevImg_gray, nextImg_gray, corners, None, **LKargs)
 
-fig = plt.figure()
-plt.plot(plot_rrsignal[::int(frame_rate / 3)])
+        # Draw Tracks (for all 1 points)
+        for i, (new, old) in enumerate(zip(nextP[status == 1],corners[status == 1])):
+            nextx, nexty = new.ravel()
+            prevx, prevy = old.ravel()
+
+            # Update x and y for measurements
+            x.append(nextx)
+            y.append(nexty)
+
+            # Create line on mask from prev pos to next pos (green)
+            mask = cv2.line(mask,pt1=(nextx,nexty),pt2=(prevx,prevy),color=(255,69,0),thickness=2)
+
+            # Create filled circle at next pos (green)
+            processedImg = cv2.circle(nextFrame,center=(nextx,nexty),radius=3,color=(255,69,0),thickness=-1)
+
+
+        # Update nf to prev
+        prevImg_gray = nextImg_gray.copy()
+        corners = nextP[status == 1].reshape(-1, 1, 2)
+
+        # Output Frame + Mask
+        output = cv2.add(processedImg, mask)
+        cv2.imshow("Sparse Optical Flow", output)
+
+        # Presses the 'q' key to break
+        if cv2.waitKey(10) & 0xFF == ord('q'):  # 10 milliseconds
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+"""DATA PROCESSING"""
+
+# Create Avg Displacement Array
+dispPerFrame = []
+for i in range(0,int(len(x)/MAX_CORNERS)):
+    disp = []
+
+    for j in range(0,MAX_CORNERS-1):
+        disp.append(((x[i+1]-x[i])**2 + (y[i+1]-y[i])**2)**0.5)
+
+    dispPerFrame.append(sum(disp))
+
+# Plot Displacement vs Frame
+plt.plot(range(0,len(dispPerFrame)), dispPerFrame)
+plt.xlabel('Frame')  # Add an x-label to the axes.
+plt.ylabel('Displacement (pixels)')  # Add a y-label to the axes.
+plt.title("Displacement vs Frame")
 plt.show()
-fig.savefig('rsignal_' + file_name + '.png')  # Use fig. here
 
-rrate = [0]
-hw = 12
-for k in range(len(rrsignal[2 * frame_rate:-2 * frame_rate])):
-    if (k < hw * frame_rate):
-        continue
-    # Pdb().set_trace()
-    fourier = abs(np.fft.fft(rrsignal[k - hw * frame_rate:k]))
-    N = fourier.size
-    fourier = fourier[:int(N / 2)]
-    freq = np.fft.fftfreq(N, d=1.0 / frame_rate)
-    freq = freq[:int(N / 2)]
-    m = max(fourier)
-    d = [i for i, j in enumerate(fourier) if j == m]
-    # Pdb().set_trace()
-    rrate.append(60 * freq[d])
+#Create Displacement per Second Array
+dispPerSecond = []
+prev = 0
+for i in range(1,len(dispPerFrame)):
+    if i%int(fps) == 0:
+        dispPerSecond.append(sum(dispPerFrame[prev:i]))
+        prev = i
 
-fig1 = plt.figure()
-plt.plot(rrate)
+# Plot Displacement vs. Time
+plt.plot(range(0, len(dispPerSecond)), dispPerSecond)
+plt.xlabel('Time (sec)')  # Add an x-label to the axes.
+plt.ylabel('Displacement (pixels)')  # Add a y-label to the axes.
+plt.title("Displacement vs Time")
 plt.show()
-fig1.savefig('bpm_' + file_name + '.png')  # Use fig. here
-
-avg_rrate = sum(rrate) / len(rrate) * 1.0
-print("Average RR rate: %d" % avg_rrate)
 
 
-#Fast breathing sample
-# n: 100, this rr: 3.5251808213334885
-# n: 200, this rr: 0.41059284205600627
-# n: 300, this rr: 2.078626586391098
-# n: 400, this rr: -0.22163602410905672
-# n: 500, this rr: 5.0181485621144555
-# n: 600, this rr: 3.6952834443784894
-# n: 700, this rr: 5.81976098438961
-# n: 800, this rr: -3.1580700624345694
-# n: 900, this rr: 2.790759134305223
+
+def get_inflection_points(arr, n=1):
+    """
+    returns inflextion points from array
+        arr: array
+        n: n-th discrete difference
+    """
+    inflections = []
+    dx = 0
+    for i, x in enumerate(np.diff(arr, n)):
+        if x >= dx and i > 0:
+            inflections.append(i*n)
+        dx = x
+    return inflections
+
+golden = (1 + 5 ** 0.5) / 2
+inflectionCount = len(get_inflection_points(dispPerSecond)) / golden
+print("Estimated Breaths in Cap: ",inflectionCount)
+print("Estimated BPM: ",inflectionCount/len(dispPerSecond))
